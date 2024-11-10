d8574e73b34119e5ccded1524c0e7fc097e6f0ff
DennyYe <dennyye32@gmail.com>
2024-11-10 12:49:27 -0500

    login add/cahnge

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,27 @@
 "use client";
-import { useState } from 'react';
+import { useState } from "react";
 import React from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
- 
-    return (
-      <div className="flex justify-center align-center h-screen bg-gray-400">
-        <div className=' bg-white'>
-          Log In
-        </div>
+  const router = useRouter();
+
+  return (
+    <div className="flex justify-center align-center h-screen bg-gray-400">
+      <div className=" bg-white flex justify-center">
+        <button
+          className="px-2 py-1 bg-white rounded font-inter font-bold text-blue-500"
+          onClick={() => router.push("/add-pin")}
+        >
+          Add Items Here
+        </button>
+        <button
+          className="px-2 py-1 bg-white rounded font-inter font-bold text-blue-500"
+          onClick={() => router.push("/")}
+        >
+          Signout
+        </button>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
