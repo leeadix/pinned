43edf6c47076e34a6cb77b5eb1ac664667d0fa35
RunningScizzors <leea509@aol.com>
2024-11-19 13:20:31 -0500

    Update Pins.tsx

--- src/app/components/Pins.tsx
+++ src/app/components/Pins.tsx
@@ -19,7 +19,8 @@ type PinsProps = {
 const Users: React.FC<PinsProps> = ({pins}) => {
     return(
         <div className={styles.pinsBox}>
-            {pins.map(pin =>(<Pin key={pin.id} pin={pin} />))}
+            {/* {pins.map(pin =>(<Pin key={pin.id} pin={pin} />))} */}
+            {pins.sort((a, b) => a.place.localeCompare(b.place)).map(pin => (<Pin key={pin.id} pin={pin} />))}
         </div>
     );
 }
